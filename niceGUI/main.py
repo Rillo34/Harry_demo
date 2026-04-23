c85e05bb300ad6ee703efb30f42259f754eeaf2f
Rillo34 <rikardanderberg01@gmail.com>
2026-04-23 21:36:52 +0200

    new structure in place9

--- niceGUI/main.py
+++ niceGUI/main.py
@@ -16,9 +16,9 @@ def route_inspiration():
 @ui.page('/consultants')
 def route_consultants():
     consultants_page()
-    
+
 if __name__ in {"__main__", "__mp_main__"}:
-    ui.run(host="0.0.0.0", port=8080)
+    ui.run(host="0.0.0.0", port=8091)
 
 
 
