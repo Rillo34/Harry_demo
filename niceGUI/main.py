b814c9298a680773eaa770146c506acfa17dde60
Rillo34 <rikardanderberg01@gmail.com>
2026-04-23 20:58:58 +0200

    new structure in place4

---
+++ niceGUI/main.py
@@ -0,0 +1,23 @@
+from nicegui import ui
+
+from pages.home import home_page
+from pages.inspiration import inspiration_page
+from pages.consultants import consultants_page
+
+
+@ui.page('/')
+def route_home():
+    home_page()
+
+@ui.page('/inspiration')
+def route_inspiration():
+    inspiration_page()
+
+@ui.page('/consultants')
+def route_consultants():
+    consultants_page()
+
+if __name__ == "__main__":
+    ui.run(host="0.0.0.0", port=8091)
+
+
